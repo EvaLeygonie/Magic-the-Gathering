34a2e4c4210103865dbf9e9308873cdca87ca970
EvaLeygonie <eva.leygonie@hotmail.fr>
2024-12-09 15:22:00 +0100

    Function merge working!
    Changed the code format and added a value="" on the card type option so it didn't get a wrong query parameter when no type was selected

--- cards.js
+++ cards.js
@@ -5,7 +5,7 @@ addEventListener("load", loadAllCards)
 async function loadAllCards() {
   const result = (await axios.get('https://api.magicthegathering.io/v1/cards')).data
 
-  const allCards = result.cards.filter(card => card.imageUrl)
+  const allCards = result.cards
   displayCards(allCards)
 }
 
@@ -13,11 +13,13 @@ function displayCards(cards) {
   cardList.innerHTML = ""
 
   cards.forEach(card => {
-    const cardImage = document.createElement('img')
-    cardImage.src = card.imageUrl
-    cardImage.alt = card.name
-    cardImage.className = 'card_image'
-    cardList.appendChild(cardImage)
+    if (card.imageUrl) {
+      const cardImage = document.createElement('img')
+      cardImage.src = card.imageUrl
+      cardImage.alt = card.name
+      cardImage.className = 'card_image'
+      cardList.appendChild(cardImage)
+    }
 })
 }
 
@@ -32,15 +34,20 @@ addEventListener("DOMContentLoaded", () => {
     const checkedColors = document.querySelectorAll("#card_colors_filter input[type='checkbox']:checked")
     const cardColorFilter = [...checkedColors].map(e => e.value)
 
+    console.log(cardTypeFilter)
+    console.log(cardNameFilter)
+    console.log(checkedColors)
+    console.log(cardColorFilter)
+
     let queryParams = []
     if (cardTypeFilter) queryParams.push(`type=${cardTypeFilter}`)
     if (cardNameFilter) queryParams.push(`name=${cardNameFilter}`)
-    if (cardColorFilter) queryParams.push(`colors=${cardColorFilter}`)
+    if (checkedColors) queryParams.push(`colors=${cardColorFilter}`)
 
     const queryString = queryParams.join("&")
 
     const result = (await axios.get(`https://api.magicthegathering.io/v1/cards?${queryString}`)).data
-    const filteredCards = result.cards.filter(card => card.imageUrl)
+    const filteredCards = result.cards
     displayCards(filteredCards)
   })
 })
