26fd798791f4a267e698c55a9fd4a568d53a7c41
EvaLeygonie <eva.leygonie@hotmail.fr>
2024-12-09 15:01:31 +0100

    Test new merge code, not working
    Tested a new way to write the code to merge the functions, does not work

--- cards.js
+++ cards.js
@@ -1,101 +1,46 @@
-//=== FUNCTIONS ===//
-
-const cards = []
-const allCards = []
 const cardList = document.querySelector("#card_list")
 
-//*! Reloads all cards before filtering every time!
-//TODO: Load all cards, when page opens
 addEventListener("load", loadAllCards)
 
-async function loadAllCards(allCards) {
+async function loadAllCards() {
   const result = (await axios.get('https://api.magicthegathering.io/v1/cards')).data
 
-  allCards = result.cards
-
-  allCards.forEach(card => {
-    if (card.imageUrl) {
-      const cardImage = document.createElement('img')
-      cardImage.src = card.imageUrl
-      cardImage.alt = card.name
-      cardImage.className = 'card_image'
-      cardList.appendChild(cardImage)
-    }
-  })
-  return allCards
+  const allCards = result.cards.filter(card => card.imageUrl)
+  displayCards(allCards)
 }
 
-addEventListener("DOMContentLoaded", (cards) => {
+function displayCards(cards) {
+  cardList.innerHTML = ""
+
+  cards.forEach(card => {
+    const cardImage = document.createElement('img')
+    cardImage.src = card.imageUrl
+    cardImage.alt = card.name
+    cardImage.className = 'card_image'
+    cardList.appendChild(cardImage)
+})
+}
+
+addEventListener("DOMContentLoaded", () => {
   const inputForm = document.querySelector("form")
 
   inputForm.addEventListener("submit", async (event) => {
     event.preventDefault()
 
-//*! Merge all filters to combine the results
-
-    //TODO: card type filter
     const cardTypeFilter = document.querySelector('select[id="card_type_filter"]').value
-    async function filterCardType(type) {
-      const result = (await axios.get(`https://api.magicthegathering.io/v1/cards?type=${type}`)).data
-
-      cardList.innerHTML = ""
-      cards = result.cards
-
-      cards.forEach(card => {
-        if (card.imageUrl) {
-          const cardImage = document.createElement('img')
-          cardImage.src = card.imageUrl
-          cardImage.alt = card.name
-          cardImage.className = 'card_image'
-          cardList.appendChild(cardImage)
-        }
-      })
-    }
-    filterCardType(cardTypeFilter)
-
-    //TODO: card name filter
     const cardNameFilter = document.querySelector('#search_card').value
-    async function filterCardName(name) {
-      const result = (await axios.get(`https://api.magicthegathering.io/v1/cards?name=${name}`)).data
+    const checkedColors = document.querySelectorAll("#card_colors_filter input[type='checkbox']:checked")
+    const cardColorFilter = [...checkedColors].map(e => e.value)
 
-      cardList.innerHTML = ""
-      cards = result.cards
+    let queryParams = []
+    if (cardTypeFilter) queryParams.push(`type=${cardTypeFilter}`)
+    if (cardNameFilter) queryParams.push(`name=${cardNameFilter}`)
+    if (cardColorFilter) queryParams.push(`colors=${cardColorFilter}`)
 
-      cards.forEach(card => {
-        if (card.imageUrl) {
-          const cardImage = document.createElement('img')
-          cardImage.src = card.imageUrl
-          cardImage.alt = card.name
-          cardImage.className = 'card_image'
-          cardList.appendChild(cardImage)
-        }
-      })
-    }
-    filterCardName(cardNameFilter)
+    const queryString = queryParams.join("&")
 
-    //TODO: Color filters works with allCards and switching
-    //*! doesn't exclusively show the colors choosen => Try filter()
-    let checkedColors = document.querySelectorAll("#card_colors_filter input[type='checkbox']:checked")
-    let cardColorFilter = [...checkedColors].map(e => e.value)
-
-    async function filterCardColor(colors) {
-      const result = (await axios.get(`https://api.magicthegathering.io/v1/cards?colors=${colors}`)).data
-
-      cardList.innerHTML = ""
-      cards = result.cards
-      /*let cardsFiltered = cards.filter((cards) => cards.colors == colors)
-      console.log(cardsFiltered)*/
-
-      cards.forEach(card => {
-        if (card.imageUrl) {
-          const cardImage = document.createElement('img')
-          cardImage.src = card.imageUrl
-          cardImage.alt = card.name
-          cardImage.className = 'card_image'
-          cardList.appendChild(cardImage)
-          }
-        })
-      }
-    filterCardColor(cardColorFilter)
-})
+    const result = (await axios.get(`https://api.magicthegathering.io/v1/cards?${queryString}`)).data
+    const filteredCards = result.cards.filter(card => card.imageUrl)
+    displayCards(filteredCards)
+  })
 })
